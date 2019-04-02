2f3972745676392c2a08a8e51707e5d1f36c4adc
Aleksander <>
2019-04-02 22:06:25 +0200

    Fibonacci number logic done.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Subscription} from "rxjs";
+import {Observable} from "rxjs/observable";
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  fibonacci = [];
+  isRunFibonacciSequence: boolean = false;
+  interval$: Subscription;
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+
+  }
+
+  runFibonacci() {
+    this.isRunFibonacciSequence = !this.isRunFibonacciSequence;
+    if(this.isRunFibonacciSequence) {
+      this.interval$ = Observable.interval(1000 * 3).subscribe(() => this.runFibonacciaa());
+    } else {
+      this.interval$.unsubscribe();
+    }
+
+  }
+
+  runFibonacciaa() {
+    let index = this.fibonacci.length;
+    if (index === 0) {
+      this.fibonacci.push(0)
+    } else if (index === 1) {
+      this.fibonacci.push(1)
+    } else {
+      this.fibonacci[index] = this.fibonacci[index - 2] + this.fibonacci[index - 1];
+    }
+  }
 }
